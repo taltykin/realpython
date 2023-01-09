ccc36fc4928cf6eb837e8c6e043304128543c905
Denis Taltykin <denis@taltykin.com>
2023-01-09 21:05:28 +0300

    Work with strings

---
+++ 04/04. 3. 7. 1. String.py
@@ -0,0 +1,8 @@
+weight = 0.2;
+animal = "newt"
+
+print(str(weight) + " kg is the weight of the " + animal)
+
+#print(f"{weight} kg is the weight of the {animal}.")
+
+#print("{} kg is the weight of the {}.".format(weight, animal))
---
+++ 04/04. 3. 8. 1. String.py
@@ -0,0 +1,8 @@
+# print("AAA".find("a"))
+
+# print("Somebody said something to Samantha.".replace("s", "x"))
+
+request = input("Введите сроку дял поиска: ")
+response = input("Введите искомый символ: ")
+
+print(request.find(response))

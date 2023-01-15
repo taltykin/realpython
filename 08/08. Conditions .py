d8d6798e94f33e8c6b4625c56f35b858735401a8
Denis Taltykin <denis@taltykin.com>
2023-01-15 20:48:25 +0300

    Conditions

---
+++ 08/08. Conditions .py
@@ -0,0 +1,13 @@
+""" Infinity loop """
+
+while True:
+    str = input("Type 'Q' or 'q' to quit: ")
+    if str == ("q" or "Q"):
+        break
+
+    """ Division on three"""
+
+# for item in range(1,51):
+#     if item % 3 == 0:
+#         continue
+#     print(item)

a14d66ea343c53b2db4c6e6e7277d950e0dbf05f
Denis Taltykin <denis@taltykin.com>
2023-01-21 21:37:15 +0300

    lists

---
+++ 09/09. 01 cardinal numbers.py
@@ -0,0 +1,15 @@
+cardinal_numbers = ("frist", "second", "third")
+
+print(cardinal_numbers[1])
+
+positionl, position2, position3 = cardinal_numbers
+
+my_name = tuple("Denis")
+# if "x" in my_name:
+#     print("Y")
+# else:
+#     print("N")
+
+my = my_name[1:]
+
+print(my)

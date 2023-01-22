6cd06fae476ee77a6c4b050b1e714913be97df9e
Denis Taltykin <denis@taltykin.com>
2023-01-22 10:55:02 +0300

    Working with lists

---
+++ 09/09. 02 lists.py
@@ -0,0 +1,20 @@
+# food = ["rice", "beans"]
+
+# food.append("broccoli")
+
+# food.extend(["bread", "pizza"])
+
+# print(food[len(food)- 1])
+
+
+
+
+breakfast = "eggs, fruit, orangejuice".split(", ")
+
+# print(len(breakfast))
+
+
+
+lengths = [len(item) for item in breakfast]
+
+print(lengths)
